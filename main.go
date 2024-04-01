84735bff73b9bfafe6fad7262ba3a2a26ca8b4b7
Ismael Nvo <mongorosalome@gmail.com>
2024-04-01 21:20:35 +0400

    update

--- main.go
+++ main.go
@@ -11,58 +11,79 @@ import (
 	"time"
 )
 
-func ReadAllfile(name string) *[]byte {
-	f, err := os.ReadFile(name)
-	if err != nil {
-		log.Fatalf("Error reading file...!%v", err)
-	}
-	return &f
+func GetDirectoryFromTerminal(src string, dst string) (string, string) {
+	var source *string
+	var destination *string
+	source = flag.String(src, "None", "")
+	destination = flag.String(dst, "None", "")
+	flag.Parse()
+	return *source, *destination
 }
-func CreateFile(Newfile *string) *os.File {
-	f, err := os.Create(*Newfile)
+func ReadDataFromfile(filename string) string {
+	//read file lines one by one line in memory
+	f, err := os.Open(filename)
 	if err != nil {
-		log.Fatalf("%v", f)
+		fmt.Printf("anable to Open File: %v", err)
 	}
-	fmt.Println("file was created...!\n", f.Name())
-	return f
+	defer f.Close()
+	buf := make([]byte, 1024)
+	var FileContent string
+	for {
+		n, err := f.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Print(err)
+		}
+		if n > 0 {
+
+			FileContent = string(buf[:n])
+		}
+	}
+	return FileContent
 }
-
-// func IsUrl(pathName string) bool {
-
-// }
-
-func GetRequest(src string, dst string) {
-	bytes := ReadAllfile(src)
-	lines := strings.Split(string(*bytes), "\n")
-
+func Createfile(filename string) *os.File {
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatalf("%v", file)
+	}
+	fmt.Printf(file.Name(), " Was been created succesfully...")
+	fmt.Println()
+	return file
+}
+func RequestFromClient(src string, destination string) {
+	datafile := ReadDataFromfile(src)
+	lines := strings.Split(datafile, "\n")
 	for _, item := range lines {
-		response, err := http.Get(item)
+		client := http.Client{}
+		resp, err := client.Get(string(item))
 		if err != nil {
 			continue
 		}
-		defer response.Body.Close()
-		body, err := io.ReadAll(response.Body)
-		google := CreateFile(&dst)
-
-		defer google.Close()
-		r, err := google.WriteString(string(body))
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
-			panic(err)
+			fmt.Print(err)
 		}
-		fmt.Print(r)
+		f := Createfile(destination)
+		defer f.Close()
+		f.WriteString(string(body))
+		defer f.Close()
+		fmt.Println(f.Name(), " Was Writed Succesfully...")
 	}
-
 }
 
-// testcommit
-
 func main() {
-
-	f1 := flag.String("src", "", "")
-	f2 := flag.String("dst", "", "")
 	start := time.Now()
-	flag.Parse()
-	GetRequest(*f1, *f2)
+	srcflag := "src"
+	dstflag := "dst"
+	s1, s2 := GetDirectoryFromTerminal(srcflag, dstflag)
+	if s1 == "None" || s2 == "None" || s1 == "" || s2 == "" {
+		fmt.Println("->Introduce correct Command line:(--src=./file.txt  --dst=./google.txt)")
+	} else {
+		RequestFromClient(s1, s2)
+	}
 	end := time.Now()
 	elapse := end.Sub(start)
 	fmt.Println("Duration time elapse:", elapse)
